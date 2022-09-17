cc49024079b59d62ada2ca79480f2a796fbf1b13
1997Hanamant <hanamantc.xworkz@gmail.com>
2022-09-17 23:41:40 +0530

    16-09-2022 assignment

---
+++ 16-09-2022-Assignment.sql
@@ -0,0 +1,27 @@
+create table pg(id int,name varchar(20),ownername varchar(20),location varchar(20),rent double,nooffloors double,noofrooms double,type varchar(20),
+noofroomsinfristfloor double,noofroomsinsecnodfloor double,noofroomsinthirdfloor double, noofpepoleinrooms double,noofbeds double,
+colorofbeds varchar(20),noofchair double,nooftables double,nooflights double,noofpans double, noofworkers double,noofdoors double);
+select * from pg;
+insert into pg values(1,'Krishna','Balu','BTM',5000,4,30,'Gents',6,6,8,50,50,'red',60,30,80,50,6,30);
+insert into pg values(2,'KrishnaGnts','Balu','BTM1stStage',5000,4,35,'Gents',6,6,8,50,50,'Yellow',60,35,80,55,6,30);
+insert into pg values(3,'KrishnaLadies','Balu','BTM1stStage',5000,4,30,'Gents',6,6,8,50,50,'red',60,30,80,50,6,30);
+insert into pg values(4,'MeganaGents','Ramu','BTM1stStage',4000,4,30,'Gents',8,7,9,54,57,'black',62,33,80,50,8,40);
+insert into pg values(5,'MeganaLadies','Balu','BTM',5000,4,30,'Ladies',6,6,8,50,50,'red',60,30,80,50,6,30);
+insert into pg values(6,'ShurtiGents','Shruti','JPNagar',6000,8,80,'Gents',6,6,8,50,55,'Green',65,30,80,50,6,30);
+insert into pg values(7,'ShurtiGents','Shruti','JPNagar',6000,8,80,'Ladies',6,6,8,50,55,'Green',65,30,80,50,6,30);
+insert into pg values(8,'RaviGents','Ravi','RRNagar',7000,8,70,'Gents',6,7,8,60,55,'White',70,50,80,60,6,30);
+insert into pg values(9,'RaviLadies','Ravi','RRNagar',8000,8,70,'Ladies',6,7,8,60,55,'White',70,50,80,60,6,50);
+insert into pg values(10,'RamuGents','Ramu','JRNagar',7000,8,70,'Gents',6,7,8,60,55,'White',70,50,80,60,6,340);
+insert into pg values(11,'RamuLadies','Ramu','JRNagar',7000,8,70,'Ladies',6,7,8,60,55,'Pink',70,50,60,60,6,30);
+insert into pg values(12,'LathaGents','Latha','PRNagar',6000,8,70,'Gents',6,7,8,60,55,'White',70,50,80,60,6,30);
+insert into pg values(13,'LathaLadies','Latha','PRNagar',7000,8,70,'Ladies',6,7,8,60,55,'White',70,50,80,60,6,30);
+insert into pg values(14,'ManojGents','Manjo','HRNagar',8000,8,70,'Gents',6,7,8,60,55,'White',70,50,80,60,6,30);
+insert into pg values(15,'ManojLadies','Manjo','HRNagar',8000,8,70,'Ladies',6,7,8,60,55,'White',70,50,80,60,6,30);
+insert into pg values(16,'SharanGents','Sharan','SRNagar',9000,8,70,'Gents',6,7,8,60,55,'Pink',70,50,80,60,6,30);
+insert into pg values(17,'SharanLadies','Sharan','SRNagar',6000,8,70,'Ladies',6,7,8,60,55,'Pink',70,50,80,60,6,30);
+insert into pg values(18,'RaghuGents','Raghu','RRRNagar',8000,8,70,'Gents',6,7,8,60,80,'Yellow',70,50,80,60,6,30);
+insert into pg values(19,'SharanLadies','Raghu','RRRNagar',8000,8,70,'Ladies',6,7,8,60,55,'Pink',70,50,80,60,6,30);
+insert into pg values(20,'SharatLadies','Sharat','SSRNagar',9000,8,70,'Ladies',6,7,8,60,55,'Pink',70,50,80,60,6,30);
+
+
+
